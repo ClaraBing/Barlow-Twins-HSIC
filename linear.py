b5bf5cc3bb3ca56c6c451b1a8763eee8f758e398
ClaraBing <bingbinl@cs.cmu.edu>
2022-05-13 10:42:18 -0400

    option to load the linear layer from another trained model; this is to check how well the features are aligned

--- linear.py
+++ linear.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 
 import pandas as pd
@@ -28,22 +29,42 @@ except Exception as e:
   print('Not using wandb. \n\n')
   USE_WANDB = False
 
+if torch.cuda.is_available():
+  torch.backends.cudnn.benchmark = True
+  device = 'cuda'
+else:
+  device = 'cpu'
+
 
 class Net(nn.Module):
-    def __init__(self, num_class, pretrained_path, dataset):
+    def __init__(self, num_class, pretrained_path, dataset, fname_linear_cls=''):
         super(Net, self).__init__()
 
         # encoder
         from model import Model
         self.f = Model(dataset=dataset).f
         # classifier
-        self.fc = nn.Linear(2048, num_class, bias=True)
+        if fname_linear_cls != '':
+          linear_state_dict = torch.load(fname_linear_cls, map_location='cpu')
+          # self.sanity_cls_weight, self.sanity_cls_bias = linear_state_dict['fc.weight'].clone().to(device), linear_state_dict['fc.bias'].clone().to(device)
+          self.cls_weight, self.cls_bias = linear_state_dict['fc.weight'], linear_state_dict['fc.bias']
+          self.cls_weight.requires_grad, self.cls_bias.requires_grad = False, False
+          self.cls_weight, self.cls_bias = self.cls_weight.to(device), self.cls_bias.to(device)
+          self.cls_weight = torch.nn.parameter.Parameter(self.cls_weight)
+          self.cls_bias = torch.nn.parameter.Parameter(self.cls_bias)
+
+          self.fc = nn.Linear(2048, 2048, bias=True)
+        else:
+          self.cls_weight, self.cls_bias = None, None
+          self.fc = nn.Linear(2048, num_class, bias=True)
         self.load_state_dict(torch.load(pretrained_path, map_location='cpu'), strict=False)
 
     def forward(self, x):
         x = self.f(x)
         feature = torch.flatten(x, start_dim=1)
         out = self.fc(feature)
+        if self.cls_weight is not None:
+          out = torch.matmul(out, self.cls_weight.T) + self.cls_bias
         return out
 
 # train or test for one epoch
@@ -55,7 +76,8 @@ def train_val(net, data_loader, train_optimizer):
     with (torch.enable_grad() if is_train else torch.no_grad()):
         bt_cnt = 0
         for data, target in data_bar:
-            data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
+            # data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
+            data, target = data.to(device), target.to(device)
             out = net(data)
             loss = loss_criterion(out, target)
 
@@ -89,6 +111,8 @@ def train_val(net, data_loader, train_optimizer):
                   'val_linear_total_correct_1': total_correct_1 / total_num * 100,
                   'val_linear_total_correct_5': total_correct_5 / total_num * 100,
                   })
+              pdb.set_trace()
+              torch.save(net.state_dict(), save_name_model)
     return total_loss / total_num, total_correct_1 / total_num * 100, total_correct_5 / total_num * 100
 
 
@@ -97,8 +121,9 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', default='cifar10', type=str, help='Dataset: cifar10 or tiny_imagenet or stl10')
     parser.add_argument('--model_path', type=str, default='results/Barlow_Twins/0.005_64_128_model.pth',
                         help='The base string of the pretrained model path')
+    parser.add_argument('--fname-linear-cls', type=str, default='', help="File name of the ckpt that will provide a linear layer.")
     parser.add_argument('--batch_size', type=int, default=512, help='Number of images in each mini-batch')
-    parser.add_argument('--epochs', type=int, default=200, help='Number of sweeps over the dataset to train')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of sweeps over the dataset to train')
 
     # optimization
     parser.add_argument('--lr', default=1e-3, type=float)
@@ -108,8 +133,10 @@ if __name__ == '__main__':
     parser.add_argument('--project', default='nonContrastive')
     parser.add_argument('--wb-name', default='default', type=str,
                         help="Run name for wandb.")
-    parser.add_argument('--wb-proj-type', default='byol', type=str,
+    parser.add_argument('--wb-proj-type', default='barlow', type=str,
                         help="For wandb filtering.")
+    parser.add_argument('--overwrite', type=int, default=0,
+                        help="Whether to overwrite an existing directory.")
 
     args = parser.parse_args()
     model_path, batch_size, epochs = args.model_path, args.batch_size, args.epochs
@@ -141,15 +168,15 @@ if __name__ == '__main__':
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=16, pin_memory=True)
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True)
 
-    model = Net(num_class=len(train_data.classes), pretrained_path=model_path, dataset=dataset).cuda()
+    model = Net(num_class=len(train_data.classes), pretrained_path=model_path, dataset=dataset, fname_linear_cls=args.fname_linear_cls).to(device)
     for param in model.f.parameters():
         param.requires_grad = False
 
     if USE_THOP:
       if dataset == 'cifar10':
-          flops, params = profile(model, inputs=(torch.randn(1, 3, 32, 32).cuda(),))
+          flops, params = profile(model, inputs=(torch.randn(1, 3, 32, 32).to(device),))
       elif dataset == 'tiny_imagenet' or dataset == 'stl10':
-          flops, params = profile(model, inputs=(torch.randn(1, 3, 64, 64).cuda(),))
+          flops, params = profile(model, inputs=(torch.randn(1, 3, 64, 64).to(device),))
       flops, params = clever_format([flops, params])
       print('# Model Params: {} FLOPs: {}'.format(params, flops))
     optimizer = optim.Adam(model.fc.parameters(), lr=lr, weight_decay=wd)
@@ -157,7 +184,15 @@ if __name__ == '__main__':
     results = {'train_loss': [], 'train_acc@1': [], 'train_acc@5': [],
                'test_loss': [], 'test_acc@1': [], 'test_acc@5': []}
 
-    save_name = args.wb_name + '_linear.csv'
+    os.makedirs('results_linear/csv', exist_ok=1)
+    save_name_csv = os.path.join('results_linear/csv', args.wb_name+'_linear.csv')
+    save_name_model = os.path.join('results_linear', args.wb_name+'_linear_model.pth')
+    if os.path.exists(save_name_model) and not args.overwrite:
+      cont = input(f"File exists: {save_name_model}. \n Continue? (y/N)")
+      if 'y' not in cont and 'Y' not in cont:
+        print("Exiting. Bye!")
+        exit()
+
 
     best_acc = 0.0
     for epoch in range(1, epochs + 1):
@@ -171,10 +206,10 @@ if __name__ == '__main__':
         results['test_acc@5'].append(test_acc_5)
         # save statistics
         data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
-        data_frame.to_csv(os.path.join('results_linear/', save_name), index_label='epoch')
-        #if test_acc_1 > best_acc:
-        #    best_acc = test_acc_1
-        #    torch.save(model.state_dict(), 'results/linear_model.pth')
+        data_frame.to_csv(save_name_csv, index_label='epoch')
+        if test_acc_1 > best_acc:
+            best_acc = test_acc_1
+            torch.save(model.state_dict(), save_name_model)
         if USE_WANDB:
           wandb.log({
             'train_loss': train_loss,
